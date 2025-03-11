977627e9e3973d6813f84372ce2cb6819a972e27
Mario <mario.gonzalez@live.u-tad.com>
2025-03-11 13:11:49 +0100

    Small Fix Controllers

--- app/Http/Controllers/api/VisitedLocationController.php
+++ app/Http/Controllers/api/VisitedLocationController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class FavoriteController extends Controller
+class VisitedLocationController extends Controller
 {
     /**
      * Display a listing of the resource.
